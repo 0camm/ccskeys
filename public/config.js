5649d97724f078c54d08cc30798e7a858fd2a6a6
0camm <garypaytonpg@gmail.com>
2026-09-12 16:14:29 -0400

    Fix API base URL by removing trailing slash
    Removed trailing slash from API base URL.

--- public/config.js
+++ public/config.js
@@ -1,4 +1,2 @@
 "use strict";
-// Set this to your Render backend's URL (no trailing slash).
-// e.g. "https://ccskeys-api.onrender.com"
-window.API_BASE_URL = "https://ccskeys.onrender.com/";
+window.API_BASE_URL = "https://ccskeys.onrender.com";
